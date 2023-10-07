9751fb2e6301c60b37e57ee0ba60caacc065e3f0
Ivan Timofeev <skat.set@gmail.com>
2023-10-08 00:15:44 +0200

    Добавил создание файлов в формате wgf4

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import os
 import argparse
+import re
+import struct
+from datetime import datetime, timedelta
+
 import aiohttp
 import asyncio
 import aiofiles
 import bz2
 import multiprocessing
+
+import numpy as np
 import requests
 import cfgrib
 from collections import namedtuple
@@ -12,13 +18,17 @@ from collections import namedtuple
 
 Grib2Data = namedtuple('Grib2Data', ['latitude_min', 'latitude_max', 'latitude_step',
                                      'longitude_min', 'longitude_max', 'longitude_step',
-                                     'multiplier', 'data'])
+                                     'multiplier', 'data', 'predict_date'])
+
 
 def extract_grib2_data(file_path):
     # NOTE: хорошо бы хранить в памяти всё и передавать, диск лишняя сущность, в но в этом API библиотеки нет готового
     # способа работы с памятью. Можно переписать, если будет необходимость ускорить общее время работы
     # NOTE: мне нужна только часть датасета, возможно для ускорения доставать только его (это еще нужно проверить)
-    # TODO: почему-то выдает ошибку "Ignoring index file '..._tot_prec.grib2.923a8.idx' older than GRIB file", нужно разобраться
+    # TODO: почему-то выдает ошибку "Ignoring index file '..._tot_prec.grib2.923a8.idx' older than GRIB file", нужно
+    # либо добавить backend_kwargs={"indexpath": ""}, чтобы игнорировать эту ошибку, либо удалять файлы индексов перед
+    # использованием (потому что они не используются, если уже созданы в предыдущем запуске). Нужно замерять как будет
+    # быстрее и есть ли разница
     datasets = cfgrib.open_datasets(file_path)
     # TODO: нужно проверить поля в файле, что они соответствуют ожидаемому нам формату, для этого задания я это опущу
     ds = datasets[0]  # TODO проверить на количество и обработать как ошибку
@@ -27,7 +37,6 @@ def extract_grib2_data(file_path):
 
     if len(ds.tp.to_numpy().shape) == 2:
         # NOTE: почему-то 07.10.2023 после 13 часов последняя 48ая запись получилась с 1 размерностью
-        print(ds)
         result_data = ds.tp.to_numpy()
     elif len(ds.tp.to_numpy().shape) == 3:
         # NOTE: мне приходят предсказания в 0, 15, 30 и 45ую минуту. В примере выгрузки в задании было показано, что за
@@ -37,22 +46,55 @@ def extract_grib2_data(file_path):
     else:
         # TODO: обработать ошибку
         return
-    return Grib2Data(latitude_min=int(ds.latitude.min() * multiplier), latitude_max=int(ds.latitude.max() * multiplier),
+    pattern = r".*(\d{4})(\d{2})(\d{2})(\d{2})_(\d{3})_2d_tot_prec.grib2"
+    match = re.match(pattern, file_path)
+    if match:
+        year, month, day, hour, offset = map(int, match.groups())
+        date = datetime(year, month, day, hour)
+        date += timedelta(hours=offset)
+    else:
+        # TODO: обработать ошибку
+        return
+    return Grib2Data(latitude_min=int(ds.latitude.min() * multiplier),
+                     latitude_max=int(ds.latitude.max() * multiplier),
                      latitude_step=int((ds.latitude.max() - ds.latitude.min()) * multiplier / len(ds.latitude)),
-                     longitude_min=int(ds.longitude.min() * multiplier), longitude_max=int(ds.longitude.max() * multiplier),
+                     longitude_min=int(ds.longitude.min() * multiplier),
+                     longitude_max=int(ds.longitude.max() * multiplier),
                      longitude_step=int((ds.longitude.max() - ds.longitude.min()) * multiplier / len(ds.longitude)),
-                     multiplier=multiplier, data=result_data)
+                     multiplier=multiplier, data=result_data, predict_date=date)
 
 
-def run_read_extracted_and_transform(extract_dir, file_urls):
-    # TODO: читать файлы параллельно
+def run_read_extracted_and_transform(extract_dir, converted, file_urls):
+    print("Началась конвертация файлов")
     grib_data_list = []
     for url in file_urls:
         grib_data_list.append(extract_grib2_data(os.path.join(extract_dir, file_name_from_url(url))))
-    # TODO: нужно проверить, что у двух соседних совпадает минимальные и максимальные координаты и шаг
-    # TODO: вычитать из следующего часа предыдущий
-    # TODO: реализовать запись в формате wgf4
-    print(grib_data_list)
+    for (i, d) in enumerate(grib_data_list):
+        if i != 0:
+            if d.latitude_step != grib_data_list[i - 1].latitude_step \
+                    or d.longitude_step != grib_data_list[i - 1].longitude_step \
+                    or d.latitude_max != grib_data_list[i - 1].latitude_max \
+                    or d.latitude_min != grib_data_list[i - 1].latitude_min \
+                    or d.longitude_max != grib_data_list[i - 1].longitude_max \
+                    or d.longitude_min != grib_data_list[i - 1].longitude_min:
+                # TODO: обработать ошибку
+                continue
+        output_dir = os.path.join(converted, d.predict_date.strftime("%Y%m%d_%H:%M_%s"))
+        os.makedirs(output_dir, exist_ok=True)
+        filepath = os.path.join(output_dir, "PRATE.wgf4")
+        with open(filepath, 'wb') as file:
+            # NOTE: В ТЗ попросили сначала указать шаг по горизонтали, потом по вертикали. По горизонтали означает
+            # справа налево (нагуглил в нескольких беседах и сам логически пришел, что идем вдоль горизонта), но
+            # это вступает в противоречие с порядком latitude и longitude указанными ранее, ведь latitude это снизу
+            # вверх. Этот момент нужно уточнить при личном общении, скорее всего здесь опечатка в ТЗ
+            header = struct.pack('7i f', d.latitude_min, d.latitude_max, d.longitude_min, d.longitude_max,
+                                 d.longitude_step, d.latitude_step, d.multiplier, -100500.0)
+            data_to_write = d.data
+            if i != 0:
+                data_to_write -= grib_data_list[i - 1].data
+            data_to_write = np.nan_to_num(data_to_write, nan=-100500.0)
+            file.write(header + data_to_write.flatten().tobytes())
+    print(f"Конвертация закончилась успешно")
 
 
 def file_name_from_url(url):
@@ -92,7 +134,7 @@ def fetch_file_list_run_processing_by_chunk(args):
     file_urls = [
         args.url + line.split('"')[1]
         for line in page_content.split("\n")
-        if line.startswith("<a href=") and 'regular-lat-lon' in line
+        if line.startswith("<a href=") and "regular-lat-lon" in line
     ]
 
     # код для отладки, использую минимум значений
@@ -112,7 +154,7 @@ def fetch_file_list_run_processing_by_chunk(args):
     for process in processes:
         process.join()
 
-    run_read_extracted_and_transform(args.extract_dir, file_urls)
+    run_read_extracted_and_transform(args.extract_dir, args.converted_dir, file_urls)
 
 
 def main():
@@ -122,11 +164,14 @@ def main():
                         help="URL сайта с файлами, по умолчанию https://opendata.dwd.de/weather/nwp/icon-d2/grib/12/tot_prec/")
     parser.add_argument("--extract_dir", default="extracted",
                         help="Каталог для извлеченных файлов, по умолчанию: extracted")
+    parser.add_argument("--converted_dir", default="icon_d2",
+                        help="Каталог для хранения сконвертированных файлов, по умолчанию: icon_d2")
     parser.add_argument("--processes", type=int, default=4,
                         help="Количество процессов для параллельной обработки, по умолчанию 4")
     args = parser.parse_args()
 
     os.makedirs(args.extract_dir, exist_ok=True)
+    os.makedirs(args.converted_dir, exist_ok=True)
 
     fetch_file_list_run_processing_by_chunk(args)
 
